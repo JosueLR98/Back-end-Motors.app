a880e9f488cc13947736d8d937e2ef97387e72b3
Josue Lopez <josue12131415@gmail.com>
2023-06-04 20:13:23 -0600

    folder aggregation.env-env.template-gitignore

--- src/database/config.js
+++ src/database/config.js
@@ -1,10 +1,11 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 const db = new Sequelize({
-  dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "root",
-  database: "motors24app",
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   logging: false,
 });
+
 module.exports = { db };
